47f9ccc418890429bf82dcfdca84211e2677c4e5
SUHAS AMARNATH <63553815+suhasamarnath@users.noreply.github.com>
2020-05-29 10:11:07 +0530

    Update Route Master App.py

--- Route Master App.py
+++ Route Master App.py
@@ -1,101 +1,104 @@
+from capstone import Driver, Employee
 from tabulate import tabulate
 
+print("-" * 30)
+print("Welcome to Route Master!")
 
-class Driver:
 
-    list1 = []
+while True:
+    print("_" * 30)
+    choice = int(input("Select your option:\n\
+		\t1. Bus Driver\n\
+		\t2. Employee\n"))
 
-    dest_list = []
-
-    def __init__(self, driver_name, driver_phone, bus_number, route, time, seats):
-        self.driver_name = driver_name
-        self.driver_phone = driver_phone
-        self.bus_number = bus_number
-        self.route = route
-        self.time = time
-        self.seats = seats
-
-    def update(self, seats, seats_booked):
-        self.seats = seats - seats_booked
-
-    def correct_phone(phno):
-        if len(str(phno)) == 10:
-            if any(obj.driver_phone == phno for obj in Driver.list1):
-                print("This phone number is already taken. Enter another one")
-                return True
+    if choice == 1:
+        driver_name = input("Name: ")
+        while True:
+            driver_phone = int(int(input("Phone Number: ")))
+            if Driver.correct_phone(driver_phone) == False:
+                break
             else:
-                return False
-        else:
-            print("Enter a valid phone number")
+                continue
 
-    def correct_bus(busno):
-        if any(obj.bus_number == busno for obj in Driver.list1):
-            print("This Bus Number already exists. Enter another number.")
-            return True
-        else:
-            return False
-
-    def seats_booking(self, obj, seatsbooked):
-        self.obj = obj
-        if (seatsbooked > obj.seats):
-            print("You have booked too many seats. Please enter again.")
-            return True
-        else:
-            obj.seats -= seatsbooked
-            if obj.seats == 0:
-                print("All seats in this bus have now been booked")
-            return False
-
-
-class Employee:
-
-    list2 = []
-
-    def __init__(self, employee_name, employee_phone, employee_department, department_id, where_to):
-        self.employee_name = employee_name
-        self.employee_phone = employee_phone
-        self.employee_department = employee_department
-        self.department_id = department_id
-        self.where_to = where_to
-
-    def correct_phone(phno):
-        if len(str(phno)) == 10:
-            if any(obj.employee_phone == phno for obj in Employee.list2):
-                print("This phone number is already taken. Enter another one")
-                return True
+        while True:
+            bus_number = input("Bus number: ").replace(" ", "").upper()
+            if Driver.correct_bus(bus_number) == False:
+                break
             else:
-                return False
-        else:
-            print("Enter a valid phone number")
+                continue
 
-    def correct_dest(dest, employee_name, employee_phone, employee_department, department_id):
-        if any(obj.route == dest for obj in Driver.list1):
-            Employee.list2.append(Employee(
-                employee_name, employee_phone, employee_department, department_id, dest))
-            return False
-        else:
-            print("Enter a valid route")
-            return True
+        route = input("Bus route: ").upper()
 
-    def correct_bus(busno):
-        if any(obj.bus_number == busno for obj in Driver.list1):
-            return False
-        else:
-            print("Enter a valid bus Number")
-            return True
+        time = input("Departure time: ")
+        seats = int(input("Seats available: "))
+        Driver.list1.append(Driver(driver_name, driver_phone,
+                                   bus_number, route, time, seats))
+        print("\n")
+        print("Your data has been recorded")
+        print("\n")
 
-    def display(temp, employee_name, employee_phone, employee_department, department_id, seatsbooked):
-        print("\nYour seats have been booked!")
-        print("\nThe Driver details are:")
-        print("\nDriver name: ", temp.driver_name)
-        print("Bus number: ", temp.bus_number)
-        print("Departue Time: ", temp.time)
-        print("Route: ", temp.route)
-        print("Driver phone number: ", temp.driver_phone)
-        print("_" * 30)
-        print("\nYour information:-")
-        print("\nName: ", employee_name)
-        print("Phone number: ", employee_phone)
-        print("Department: ", employee_department)
-        print("ID: ", department_id)
-        print("Total seats booked: ", seatsbooked)
+    if choice == 2:
+        employee_name = input("Name: ")
+
+        while True:
+            employee_phone = int(input("Enter Phone number: "))
+            if Employee.correct_phone(employee_phone) == False:
+                break
+            else:
+                continue
+
+        employee_department = input("Department: ")
+        department_id = input("Depatment id: ")
+        print("Buses are travelling to the following routes ( please enter route available in the list ): ")
+
+        table = []
+        for i in Driver.list1:
+            if i.seats == 0:
+                continue
+            table.append([i.route, i.driver_name,
+                          i.driver_phone, i.bus_number, i.time, i.seats])
+        print(tabulate(table, headers=["Route", "Driver Name", "Driver PhNo",
+                                       "Bus Number", "Time of Departure", " Seats Available"], tablefmt="psql"))
+
+        while True:
+            where_to = input("Enter Route: ").upper()
+            if Employee.correct_dest(where_to, employee_name, employee_phone, employee_department, department_id) == False:
+                break
+            else:
+                continue
+
+        print("Buses available: ")
+
+        headers = ["Driver Name", "Bus No.",
+                   "Time of Departure", "Seats available"]
+        tables = []
+        for obj in Driver.list1:
+            if obj.route == where_to:
+                if obj.seats == 0:
+                    continue
+                tables.append(
+                    [obj.driver_name, obj.bus_number, obj.time, obj.seats])
+        print(tabulate(tables, headers=headers, tablefmt="psql"))
+        print("\n")
+        while True:
+            busno = input("Enter the bus no. that you want to travel in: ").replace(
+                " ", "").upper()
+            if Employee.correct_bus(busno) == False:
+                break
+            else:
+                continue
+
+        for obj in Driver.list1:
+            if obj.bus_number == busno:
+                temp = obj
+                while True:
+                    seatsbooked = int(
+                        input("Enter the number of seats you want to book: "))
+                    if obj.seats_booking(obj, seatsbooked) == False:
+                        break
+                    else:
+                        continue
+                break
+
+        Employee.display(temp, employee_name, employee_phone,
+                         employee_department, department_id, seatsbooked)
